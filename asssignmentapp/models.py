17ccf735576a4f85b6f512c5815140a3afac3496
anshul akotiya <anshul90akotiya@gmail.com>
2022-12-28 14:46:46 +0530

    Assignment_2

--- asssignmentapp/models.py
+++ asssignmentapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class Counting(models.Model):
-    like = models.PositiveBigIntegerField(default=0)
-    dislike = models.PositiveBigIntegerField(default=0)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
